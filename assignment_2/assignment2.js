ca229b5cea95e06b6de81e082b2397e6c4fafcb7
mjgalle <mjgallegosv05@gmail.com>
2017-10-02 11:01:16 -0500

    HTML BASICS PRECOURSE exercises

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,11 +2,15 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-var number = 0 ;
+var number = 1 ;
     while (number <= 100){
         console.log(number);
         number = number +1;
     }
+/* the on you see ontop is the one I thought was correct
+/* later i realized there is a short hand for it
+
+
 
 
 
@@ -22,3 +26,8 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+for (var number = "#";
+number.length <7;
+number +="#")
+console.log(number);
